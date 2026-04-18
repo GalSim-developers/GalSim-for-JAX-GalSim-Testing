61c6ce193569d78d8575cb149800dc6cf423729a
beckermr <becker.mr@gmail.com>
2026-04-18 07:45:33 -0500

    test: try this

--- tests/test_interpolatedimage.py
+++ tests/test_interpolatedimage.py
@@ -1662,16 +1662,16 @@ def test_ne(ref):
     unequal InterpolatedImages or InterpolatedKImages may be the same due to truncation.
     """
     final, ref_image = ref
-    obj1 = galsim.InterpolatedImage(ref_image, flux=20, calculate_maxk=False, calculate_stepk=False)
+    obj1 = galsim.InterpolatedImage(ref_image, flux=2e24, calculate_maxk=False, calculate_stepk=False)
 
     # Copy ref_image and perturb it slightly in the middle, away from where the InterpolatedImage
     # repr string will report.
     perturb_image = ref_image.copy()
     if is_jax_galsim():
-        perturb_image._array = perturb_image._array.at[64, 64].set(perturb_image._array[64, 64] * 1.001)
+        perturb_image._array = perturb_image._array.at[64, 64].set(perturb_image._array[64, 64] * 100)
     else:
-        perturb_image.array[64, 64] *= 1.001
-    obj2 = galsim.InterpolatedImage(perturb_image, flux=20, calculate_maxk=False, calculate_stepk=False)
+        perturb_image.array[64, 64] *= 100
+    obj2 = galsim.InterpolatedImage(perturb_image, flux=2e24, calculate_maxk=False, calculate_stepk=False)
 
     with galsim.utilities.printoptions(threshold=128*128):
         assert repr(obj1) != repr(obj2), "Reprs unexpectedly agree: %r"%obj1
