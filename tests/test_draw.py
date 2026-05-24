174bdd3729cea8d4100dcaa700c38f60a9000eea
Matthew R. Becker <beckermr@users.noreply.github.com>
2026-05-24 15:18:59 -0500

    Apply suggestion from @beckermr

--- tests/test_draw.py
+++ tests/test_draw.py
@@ -391,7 +391,7 @@ def test_drawImage():
     # These options are invalid unless metho=phot
     assert_raises(TypeError, obj.drawImage, image=im10, n_photons=3)
     assert_raises(TypeError, obj.drawImage, rng=galsim.BaseDeviate(234))
-    assert_raises(ValueError, obj.drawImage, max_extra_noise=23)
+    assert_raises((TypeError, ValueError), obj.drawImage, max_extra_noise=23)
     assert_raises(TypeError, obj.drawImage, poisson_flux=True)
     assert_raises(TypeError, obj.drawImage, maxN=10000)
     assert_raises(TypeError, obj.drawImage, save_photons=True)
